4e5520368b533908bf9b8b940cc592c3607d7753
Jacek <jacek@Jacek-Home.(none)>
2014-02-03 22:38:31 +0100

    Eclipse project created. Hello World :)

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Jacek R
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+
+#include <GL/glew.h>
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+
+	return 0;
+}
